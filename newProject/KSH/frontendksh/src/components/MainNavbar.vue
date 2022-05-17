7dc40d166aed7014a463dace840a9d441c941a49
midnightrebel <grandcombo258@gmail.com>
2022-05-17 09:36:39 +0400

    addd main page

--- newProject/KSH/frontendksh/src/components/MainNavbar.vue
+++ newProject/KSH/frontendksh/src/components/MainNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Генерация групп КШ</a>
       <button
         class="navbar-toggler"
         type="button"
@@ -16,7 +16,9 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#/home"
+              >Домашняя страница</a
+            >
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
@@ -30,13 +32,12 @@
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
-              Dropdown
+              Генерация группы
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#">Contester</a></li>
+              <li><a class="dropdown-item" href="#">Гугл-форма</a></li>
+              <li><a class="dropdown-item" href="#">Excel</a></li>
             </ul>
           </li>
           <li class="nav-item">
@@ -49,15 +50,25 @@
             >
           </li>
         </ul>
-        <form class="d-flex">
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <!-- <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            /> -->
+        <button
+          class="btn btn-outline-success me-2"
+          type="submit"
+          href="#"
+          position="relative"
+        >
+          Войти
+        </button>
+        <button class="btn btn-outline-success" type="submit" href="#">
+          Зарегистрироваться
+        </button>
+        <!-- </form> -->
       </div>
     </div>
   </nav>
--- newProject/KSH/frontendksh/src/main.js
+++ newProject/KSH/frontendksh/src/main.js
@@ -1,7 +1,8 @@
 import App from "./App.vue";
 import router from "./router";
 import { createApp } from "vue";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
