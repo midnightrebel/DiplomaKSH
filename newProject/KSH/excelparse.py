a051e7de486137dc528a7ba49f916de96f3e8d52
midnight <grandcombo258@gmail.com>
2022-05-18 21:26:01 +0400

    update in excelparse.py

--- newProject/KSH/excelparse.py
+++ newProject/KSH/excelparse.py
@@ -2,19 +2,22 @@ from openpyxl import load_workbook
 
 def excelparsing(link):
     book = load_workbook(link)
-    sheets = book.sheetnames
-    for sheet in sheets:
-        sheet = book.active
-
+    sheets = book.worksheets
     FIO = []
     school = []
+    subject = []
     result = ''
-    for row in sheet.rows:
-        if row[0].value != None and row[0].value != "ФИО":
-            FIO.append(row[0].value)
-        if row[1].value != None and row[1].value != "Школа":
-            school.append(row[1].value)
-    print("{}\n{} \t\n{} ".format(sheet.title,'\n'.join(FIO),'\n'.join(school)))
+    for sheet in sheets:
+        subject.append(sheet)
+    for i in range(0,len(subject)):
+        sheet = subject[0]
+        for row in sheet.rows:
+            if row[0].value != None and row[0].value != "ФИО":
+                FIO.append(row[0].value)
+            if row[1].value != None and row[1].value != "Школа":
+                school.append(row[1].value)
+    print('\n'.join(FIO))
+    print('\n'.join(school))
     return result
 link = str(input("Вставьте ссылку на документ:"))
 excelparsing(link)
